9bc89a94bd83dfd797b343d5deac4da84600f75f
aelson <aelson.alves@gmail.com>
2024-05-13 23:50:21 -0230

    The Merge Sort Algorithm sorting a unique array

--- src/tech/aelson/algorithms/sort/merge_sort/MergeSort.groovy
+++ src/tech/aelson/algorithms/sort/merge_sort/MergeSort.groovy
@@ -3,7 +3,7 @@ package tech.aelson.algorithms.sort.merge_sort
 import tech.aelson.algorithms.model.Grade
 
 class MergeSort {
-    static Grade[] execute(Grade[] firstArray, Grade[] secondArray) {
+    static Grade[] mergeTwoArrays(Grade[] firstArray, Grade[] secondArray) {
         def total = firstArray.length + secondArray.length
         def merged = new Grade[total]
         def currentOfFirstArray = 0
@@ -41,4 +41,48 @@ class MergeSort {
         }
         return merged
     }
+
+    static Grade[] sortOneArray(Grade[] array, int start, int middle, int end) {
+        def total = array.length
+        def sorted = new Grade[total - start]
+        def sortedIndex = 0
+        def firstPartIndex = start
+        def secondPartIndex = middle
+        while (firstPartIndex < middle && secondPartIndex < end) {
+            println "Comparing ${array[firstPartIndex].studentName} (${array[firstPartIndex].result}) with ${array[secondPartIndex].studentName} (${array[secondPartIndex].result})"
+
+            if (array[firstPartIndex].result < array[secondPartIndex].result) {
+                println "-> Inserting ${array[firstPartIndex].studentName} (${array[firstPartIndex].result}) on the position $sortedIndex"
+                sorted[sortedIndex] = array[firstPartIndex]
+                firstPartIndex++
+            } else {
+                println "-> Inserting ${array[secondPartIndex].studentName} (${array[secondPartIndex].result}) on the position $sortedIndex"
+                sorted[sortedIndex] = array[secondPartIndex]
+                secondPartIndex++
+            }
+            println "------------------------------------"
+            sortedIndex++
+        }
+        while (firstPartIndex < middle) {
+            println "-> Inserting ${array[firstPartIndex].studentName} (${array[firstPartIndex].result}) on the position $sortedIndex because it is left over from the first part of the array"
+            sorted[sortedIndex] = array[firstPartIndex]
+            firstPartIndex++
+            sortedIndex++
+        }
+        while (secondPartIndex < end) {
+            println "-> Inserting ${array[secondPartIndex].studentName} (${array[secondPartIndex].result}) on the position $sortedIndex because it is left over from the second part of the array"
+            sorted[sortedIndex] = array[secondPartIndex]
+            secondPartIndex++
+            sortedIndex++
+        }
+        if (start > 0) {
+            println "Rebuilding the original array keeping the initial object(s) not ordered (because the start is greater than 0)"
+            (0..<sortedIndex).each { indexOfMerged ->
+                println "-> Inserting ${sorted[indexOfMerged].studentName} (${sorted[indexOfMerged].result}) on the position ${start + indexOfMerged}"
+                array[start + indexOfMerged] = sorted[indexOfMerged]
+            }
+        }
+        return array
+    }
+
 }
--- src/tech/aelson/algorithms/sort/merge_sort/MergeSortOneArrayTester.groovy
+++ src/tech/aelson/algorithms/sort/merge_sort/MergeSortOneArrayTester.groovy
@@ -3,26 +3,22 @@ package tech.aelson.algorithms.sort.merge_sort
 import tech.aelson.algorithms.model.Grade
 import tech.aelson.algorithms.util.Util
 
-class MergeSortTester {
+class MergeSortOneArrayTester {
     static void main(String[] args) {
-        Grade[] firstArray = [
+        Grade[] grades = [
                 new Grade("andre", 4),
                 new Grade("mariana", 5),
                 new Grade("carlos", 8.5),
-                new Grade("paulo", 9)
-        ]
-
-        Grade[] secondArray = [
+                new Grade("paulo", 9),
                 new Grade("jonas", 3),
                 new Grade("juliana", 6.7),
                 new Grade("guilherme", 7),
                 new Grade("lucia", 9.3),
                 new Grade("ana", 10)
         ]
-        Util.printGradesArray("First array: ", firstArray)
-        Util.printGradesArray("Second array: ", secondArray)
+        Util.printGradesArray("Grades array: ", grades)
 
-        def rank = MergeSort.execute(firstArray, secondArray)
-        Util.printGradesArray("Merged array: ", rank)
+        def rank = MergeSort.sortOneArray(grades, 0, 4, grades.length)
+        Util.printGradesArray("Sorted array: ", rank)
     }
 }
